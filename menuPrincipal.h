138370bd542f9838d4ffc8095f32ecddfc32751e
Mauri-utn <56610750+Mauri-utn@users.noreply.github.com>
2019-10-29 23:03:54 -0300

    Update menuPrincipal.h

--- menuPrincipal.h
+++ menuPrincipal.h
@@ -10,6 +10,7 @@ int menuPrincipal(int);
 #include "menuHabitaciones.h"
 #include "menuReservas.h"
 #include "recepcion.h"
+#include "productos.h"
 
 
 int menuPrincipal(int permisos){
@@ -29,11 +30,12 @@ int menuPrincipal(int permisos){
         cout << "\t\t3) CHECK OUT       " << endl;
         cout << "\t\t4) HABITACIONES    " << endl;
         cout << "\t\t5) CLIENTES    --->" << endl;
-        cout << "\t\t6) EMPLEADOS       " << endl;
-        cout << "\t\t7) MANTENIMIENTO   " << endl;
-        cout << "\t\t8) LIMPIEZA        " << endl;
-        cout << "\t\t9) CONFIGURACION   " << endl;
-        cout << "\t\t10)REPORTES        " << endl;
+        cout << "\t\t6) PRODUCTOS   --->(tira error)" << endl;
+        cout << "\t\t7) EMPLEADOS       " << endl;
+        cout << "\t\t8) MANTENIMIENTO   " << endl;
+        cout << "\t\t9) LIMPIEZA        " << endl;
+        cout << "\t\t10)CONFIGURACION   " << endl;
+        cout << "\t\t11)REPORTES        " << endl;
         cout << "\t\t0) SALIR/ATRÁS --->" << endl;
         cout << endl << "Opción: ";
         cin >> opcion;
@@ -68,15 +70,18 @@ int menuPrincipal(int permisos){
       case '6':
       case 'f':
       case 'F':
+          menuProductos();
         break;
       case '7':
       case 'g':
       case 'H':
+
         break;
         case '0':
       return 0;
         break;
         default:
+            mensajes(2);
             break;
         }
         cout << endl;
@@ -91,4 +96,3 @@ int menuPrincipal(int permisos){
 
 
 #endif // MENUPRINCIPAL_H_INCLUDED
-
