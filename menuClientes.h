7b6765fbbf0c93fcb10f3da1328915d32ad76a36
Mauri-utn <56610750+Mauri-utn@users.noreply.github.com>
2019-11-02 14:45:39 -0300

    Update menuClientes.h

--- menuClientes.h
+++ menuClientes.h
@@ -128,6 +128,9 @@ public:
     void setAnio(int nuevoAnio){
     anio=nuevoAnio;
     }
+
+    /// sobrecarga de operadores
+    bool operator==(Fecha);
 };
 
 void Fecha::cargar(){
@@ -810,24 +813,34 @@ void listarClientes(){
 
 /*void heapsortClientes(Cliente *v, long cant){
 
-    for(k=n;k>0;k--)
-    {
-        for(i=1;i<=k;i++)
-        {
-            item=A[i];
-            j=i/2;
-            while(j>0 && A[j]<item)
-            {
-                A[i]=A[j];
-                i=j;
-                j=j/2;
-            }
-            A[i]=item;
-        }
-        temp=A[1];
-        A[1]=A[k];
-        A[k]=temp;
-    }
+    int A[max],j,item,temp,i,k,n;
+	cout<<"Ingresa la cantidad de elementos del arreglo: ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	cin >> A[i];
+
+	for(k=n;k>0;k--)
+	{
+		for(i=1;i<=k;i++)
+		{
+			item=A[i];
+			j=i/2;
+			while(j>0 && A[j]<item)
+			{
+				A[i]=A[j];
+				i=j;
+				j=j/2;
+			}
+			A[i]=item;
+		}
+		temp=A[1];
+		A[1]=A[k];
+		A[k]=temp;
+	}
+	cout<<"El orden es:"<<endl;
+	for(i=1;i<=n;i++)
+	cout<<A[i] << endl;
+	return 0;
 
 
 }*/
