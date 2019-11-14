c1c496a758d09169e8c4913820b52893a06924d2
Mauri-utn <56610750+Mauri-utn@users.noreply.github.com>
2019-11-14 00:06:11 -0300

    Update misFunciones.h

--- misFunciones.h
+++ misFunciones.h
@@ -5,7 +5,7 @@ void ponerMatEnCero(float [20][3]);
 void borrarPantalla();
 void pausa();
 void fechaHora();
-bool cadenaVacia(char*);
+bool cadenaVacia(const char*);
 
 void borrarPantalla()
 {
@@ -26,27 +26,33 @@ void pausa()
 #endif
 
 }
-void ponerVecEnCero(int*v,int tam){
-int i;
-for(i=0;i<tam;i++){
-    v[i]=0;
-}
+void ponerVecEnCero(int*v,int tam)
+{
+    int i;
+    for(i=0; i<tam; i++)
+    {
+        v[i]=0;
+    }
 
 };
 
-void ponerMatEnCero(float mat[20][3]){
+void ponerMatEnCero(float mat[20][3])
+{
 
     int i,x;
-    for(i=0;i<20;i++){
-        for (x=0;x<3;x++){
-                mat[i][x]=0;
-            }
+    for(i=0; i<20; i++)
+    {
+        for (x=0; x<3; x++)
+        {
+            mat[i][x]=0;
+        }
+
+    }
 
 }
 
-}
-
-void fechaHora(){
+void fecha()
+{
 
 
     time_t tiempo;
@@ -55,43 +61,87 @@ void fechaHora(){
     tmPtr = localtime(&tiempo);
     ///FECHA ACTUAL tmPtr->tm_mday, tmPtr->tm_mon+1, 1900+tmPtr->tm_year
     cout << tmPtr->tm_mday <<"/"<<tmPtr->tm_mon+1<<"/"<<1900+tmPtr->tm_year<<endl;
+    ///cout << tmPtr->tm_hour<<":"<<tmPtr->tm_min<<":"<<tmPtr->tm_sec<<endl;///HORA
+
+}
+
+void hora()
+{
+
+
+    time_t tiempo;
+    struct tm *tmPtr;
+    tiempo = time(NULL);
+    tmPtr = localtime(&tiempo);
+    ///FECHA ACTUAL tmPtr->tm_mday, tmPtr->tm_mon+1, 1900+tmPtr->tm_year
+    ///cout << tmPtr->tm_mday <<"/"<<tmPtr->tm_mon+1<<"/"<<1900+tmPtr->tm_year<<endl;///FECHA
     cout << tmPtr->tm_hour<<":"<<tmPtr->tm_min<<":"<<tmPtr->tm_sec<<endl;
 
 }
 
 
-void mensajes(int opcion){
+void mensajes(int opcion)
+{
 
-switch(opcion){
-case 1:
-    cout << "ERROR AL ABRIR ARCHIVO"<< endl;
-    pausa();
-    break;
-case 2:
-    cout << "OPCIÓN INVALIDA"<< endl;
-    pausa();
-    break;
-case 3:
-    cout << "EL CAMPO NO PUEDE ESTAR VACÍO"<< endl;
-    pausa();
-    break;
+    switch(opcion)
+    {
+    case 1:
+        cout << "ERROR AL ABRIR ARCHIVO"<< endl;
+        pausa();
+        break;
+    case 2:
+        cout << "OPCIÓN INVALIDA"<< endl;
+        pausa();
+        break;
+    case 3:
+        cout << "EL CAMPO NO PUEDE ESTAR VACÍO"<< endl;
+        pausa();
+        break;
 
-}
-
-}
-
-bool cadenaVacia(char * cadena){
-
-if (strlen(cadena)==0){return true;}
-else return false;
+    }
 
 }
 
-void limpiarBuffer(){
+bool cadenaVacia(const char * cadena)
+{
+
+    if (strlen(cadena)==0)
+    {
+        return true;
+    }
+    else
+        return false;
+
+}
+
+void limpiarBuffer()
+{
 
     fflush(stdin);
 }
 
 
-#endif // MISFUNCIONES_H_INCLUDED
+bool salir(){
 
+    while(1){
+        char selec[5];
+        cout << "Salir(si/no)" << endl;
+        limpiarBuffer();
+        cin.getline(selec,5);
+        if(strcmp(selec,"si")==0)
+            return true;
+        if(strcmp(selec,"SI")==0)
+            return true;
+        if(strcmp(selec,"no")==0)
+            return false;
+        if(strcmp(selec,"NO")==0)
+            return false;
+        mensajes(1);
+
+    }
+}
+
+
+
+
+#endif // MISFUNCIONES_H_INCLUDED
